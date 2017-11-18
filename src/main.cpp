92cbf3016ca9f2aabd0839adfe3993a1258bf6d0
turuslan <turuslan.devbox@gmail.com>
2017-11-18 22:23:15 +0300

    add `CmdArgsParser`

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/IR/IRBuilder.h"
@@ -14,6 +15,7 @@
 #include "llvm/IR/LegacyPassManager.h"
 #include "AST/ast.h"
 #include "codegen/codegen_visitor.h"
+#include "lib/cxxopts.hpp"
 
 llvm::LLVMContext TheContext;
 llvm::IRBuilder<> Builder(TheContext);
@@ -32,6 +34,52 @@ llvm::Function *codegen_example()
 
     return f;
 }
+
+class CmdArgsParser {
+public:
+    std::string input;
+    std::string output;
+
+    bool printHelp;
+    std::string helpText;
+
+    bool error;
+
+public:
+    CmdArgsParser(int argc, char *argv[]) {
+        cxxopts::Options options(argv[0]);
+        options
+            .positional_help("input")
+            .show_positional_help();
+        options.add_options()
+            ("h,help", "print help")
+            ("o,output", "output file", cxxopts::value<std::string>())
+            ("i,input", "input file", cxxopts::value<std::string>());
+        options.parse_positional("input");
+
+        auto result = options.parse(argc, argv);
+
+        this->error = false;
+
+        this->printHelp = result.count("help");
+        this->helpText = options.help();
+        if (this->printHelp) {
+            return;
+        }
+
+        if (!result.count("input")) {
+            this->error = true;
+            return;
+        }
+        this->input = result["input"].as<std::string>();
+
+        if (!result.count("output")) {
+            this->error = true;
+            return;
+        }
+        this->output = result["output"].as<std::string>();
+    }
+};
 
 int main() 
 {
