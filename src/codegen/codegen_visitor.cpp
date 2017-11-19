c5496267412fc70734e5c159b3af54abe9216fad
Nikita Alekseev <n.alekseev2104@gmail.com>
2017-11-19 16:57:13 +0300

    Fixed compilation issues

--- src/codegen/codegen_visitor.cpp
+++ src/codegen/codegen_visitor.cpp
@@ -1,7 +1,71 @@
 #include "codegen_visitor.h"
 #include <iostream>
 
+CodegenVisitor::CodegenVisitor(const std::string& filename): Builder{TheContext}, output{filename, EC, llvm::sys::fs::F_None}
+{
+    TheModule = llvm::make_unique<llvm::Module>("my cool jit", TheContext);
+    auto TargetTriple = llvm::sys::getDefaultTargetTriple();
+    llvm::InitializeAllTargetInfos();
+    llvm::InitializeAllTargets();
+    llvm::InitializeAllTargetMCs();
+    llvm::InitializeAllAsmParsers();
+    llvm::InitializeAllAsmPrinters();
+    std::string Error;
+    auto Target = llvm::TargetRegistry::lookupTarget(TargetTriple, Error);
+    if (!Target) {
+        llvm::errs() << Error;
+    }
+
+    auto CPU = "generic";
+    auto Features = "";
+    llvm::TargetOptions opt;
+    auto RM = llvm::Optional<llvm::Reloc::Model>();
+    targetMachine = Target->createTargetMachine(TargetTriple, CPU, Features, opt, RM);
+
+    TheModule->setDataLayout(targetMachine->createDataLayout());
+    TheModule->setTargetTriple(TargetTriple);
+}
+
+void CodegenVisitor::generate()
+{
+    llvm::legacy::PassManager pass;
+    auto FileType = llvm::TargetMachine::CGFT_ObjectFile;
+    
+    targetMachine->addPassesToEmitFile(pass, output, FileType);
+    
+    pass.run(*TheModule);
+    output.flush();
+}
+
 void CodegenVisitor::visit(Prototype& p)
 {
-    std::cout << "visiting " << p.getName() << "\n";
+    llvm::FunctionType *ft = llvm::FunctionType::get(llvm::Type::getVoidTy(TheContext), false);
+    llvm::Function *f = llvm::Function::Create(ft, llvm::Function::ExternalLinkage, p.getName(), TheModule.get());
+    llvm::BasicBlock *BB = llvm::BasicBlock::Create(TheContext, "entry", f);
+    Builder.SetInsertPoint(BB);
+    Builder.CreateRetVoid();
+    verifyFunction(*f);
 }
+
+void CodegenVisitor::visit(Argument& node) {}
+void CodegenVisitor::visit(Array& node) {}
+void CodegenVisitor::visit(ArrayDecl& node) {}
+void CodegenVisitor::visit(Assignment& node) {}
+void CodegenVisitor::visit(Binary& node) {}
+void CodegenVisitor::visit(Boolean& node) {}
+// void CodegenVisitor::visit(Factor& node) {}
+void CodegenVisitor::visit(For& node) {}
+void CodegenVisitor::visit(If& node) {}
+void CodegenVisitor::visit(Integer& node) {}
+void CodegenVisitor::visit(Real& node) {}
+void CodegenVisitor::visit(RecordDecl& node) {}
+// void CodegenVisitor::visit(Relation& node) {}
+void CodegenVisitor::visit(Routine& node) {}
+void CodegenVisitor::visit(RoutineCall& node) {}
+// void CodegenVisitor::visit(Sign& node) {}
+// void CodegenVisitor::visit(Type& node) {}
+void CodegenVisitor::visit(TypeDecl& node) {}
+void CodegenVisitor::visit(Unary& node) {}
+void CodegenVisitor::visit(Var& node) {}
+void CodegenVisitor::visit(Variable& node) {}
+void CodegenVisitor::visit(While& node) {}
--- src/codegen/codegen_visitor.h
+++ src/codegen/codegen_visitor.h
@@ -1,8 +1,58 @@
+#include <memory>
+
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Verifier.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Target/TargetOptions.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Support/TargetRegistry.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/IR/LegacyPassManager.h"
+
 #include "../visitor.h"
 #include "../AST/prototype.h"
 
+// using namespace llvm;
+
 class CodegenVisitor : Visitor
 {
+private:
+    llvm::LLVMContext TheContext;
+    llvm::IRBuilder<> Builder;
+    std::unique_ptr<llvm::Module> TheModule;
+    llvm::TargetMachine* targetMachine;
+    llvm::raw_fd_ostream output;
+    std::error_code EC;
+
 public:
+    CodegenVisitor(const std::string& filename);
+    CodegenVisitor() = delete;
     void visit(Prototype& p);
+    void visit(Argument& node);
+    void visit(Array& node);
+    void visit(ArrayDecl& node);
+    void visit(Assignment& node);
+    void visit(Binary& node);
+    void visit(Boolean& node);
+    // void visit(Factor& node);
+    void visit(For& node);
+    void visit(If& node);
+    void visit(Integer& node);
+    void visit(Real& node);
+    void visit(RecordDecl& node);
+    // void visit(Relation& node);
+    void visit(Routine& node);
+    void visit(RoutineCall& node);
+    // void visit(Sign& node);
+    // void visit(Type& node);
+    void visit(TypeDecl& node);
+    void visit(Unary& node);
+    void visit(Var& node);
+    void visit(Variable& node);
+    void visit(While& node);
+    void generate();
 };
